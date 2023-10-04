81a00433a38810f7d95801c7f88fd92f982cc806
camilorojas91 <camilorojas20@gmail.com>
2023-10-03 20:35:09 -0500

    se agregan graficas

--- project2.R
+++ project2.R
@@ -7,7 +7,13 @@ SCC <- readRDS("./data/Source_Classification_Code.rds")
 
 ##### plot 1 #####
 
-hist(NEI$year, main = "PM2.5 general EEUU", xlab = "year")
+jpeg("plot1.jpeg", quality = 100)
+
+plot1 <- hist(NEI$year, main = "PM2.5 general EEUU", xlab = "year")
+
+dev.off()
+
+
 
 #No the emitions are increasing 
 
@@ -16,14 +22,19 @@ hist(NEI$year, main = "PM2.5 general EEUU", xlab = "year")
 NEI2 <- NEI %>% 
   filter(fips == "24510")
 
+jpeg("plot2.jpeg", quality = 100)
+
 hist(NEI2$year, main = "PM2.5 Baltimore City", xlab = "year")
+dev.off()
+
 
 #####plot 3 ####
+jpeg("plot3.jpeg", quality = 100)
 
 ggplot(NEI2, aes(x = year))+
   geom_bar()+
   facet_wrap(.~type)
-
+dev.off()
 ##### plot 4 ####
 
 SCC$SCC <- as.character(SCC$SCC)
@@ -32,22 +43,24 @@ base_total <- full_join(NEI, SCC, by = "SCC")
 
 base_total$coal <- ifelse(str_detect(base_total$SCC.Level.Three, "coal") == TRUE, 1,0)
 
+jpeg("plot4.jpeg", quality = 100)
 ggplot(base_total, 
        aes(x = year, y = coal))+
   geom_col()
-
+dev.off()
 ##### plot 5 ####
-
+jpeg("plot5.jpeg", quality = 100)
 ggplot(NEI2,
        aes(x = year, y = Emissions))+
   geom_col()
-
+dev.off()
 #### plot 6 #####
 
 NEI3 <- NEI %>% 
   filter(fips == c("24510", "06037"))
-
+jpeg("plot6.jpeg", quality = 100)
 ggplot(NEI3,
        aes(x = year, y = Emissions))+
   geom_col()+
   facet_wrap(.~fips)
+dev.off()
